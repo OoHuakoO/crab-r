39af737615abdc5ca8ef6b3941c7e771e5f39b68
OoHuakoO <komsark_@hotmail.co.th>
2024-07-17 21:16:44 +0700

    add font k2d

--- src/theme/font.tsx
+++ src/theme/font.tsx
@@ -1,25 +1,25 @@
 export const fontConfig = {
     // Display
     displaySmall: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 36,
         letterSpacing: 0,
         lineHeight: 44
     },
     displayMedium: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 45,
         letterSpacing: 0,
         lineHeight: 52
     },
     displayLarge: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 57,
         letterSpacing: 0,
         lineHeight: 64
     },
     displayPassword: {
-        fontFamily: 'DMSans-Light',
+        fontFamily: 'K2D-Regular',
         fontSize: 57,
         letterSpacing: 0,
         lineHeight: 64
@@ -27,19 +27,19 @@ export const fontConfig = {
 
     // Headline
     headlineSmall: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 24,
         letterSpacing: 0,
         lineHeight: 32
     },
     headlineMedium: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 28,
         letterSpacing: 0,
         lineHeight: 36
     },
     headlineLarge: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 32,
         letterSpacing: 0,
         lineHeight: 40
@@ -47,19 +47,19 @@ export const fontConfig = {
 
     // Title
     titleSmall: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 14,
         letterSpacing: 0.1,
         lineHeight: 20
     },
     titleMedium: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 16,
         letterSpacing: 0.15,
         lineHeight: 24
     },
     titleLarge: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 22,
         letterSpacing: 0,
         lineHeight: 28
@@ -67,38 +67,38 @@ export const fontConfig = {
 
     // Label
     labelSmall: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 11,
         letterSpacing: 0.5,
         lineHeight: 16
     },
     labelMedium: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 12,
         letterSpacing: 0.5,
         lineHeight: 16
     },
     labelLarge: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 14,
         letterSpacing: 0.1,
         lineHeight: 20
     },
     // Body
     bodySmall: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 12,
         letterSpacing: 0.4,
         lineHeight: 16
     },
     bodyMedium: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 14,
         letterSpacing: 0.25,
         lineHeight: 20
     },
     bodyLarge: {
-        fontFamily: 'DMSans-Regular',
+        fontFamily: 'K2D-Regular',
         fontSize: 16,
         letterSpacing: 0.15,
         lineHeight: 24
