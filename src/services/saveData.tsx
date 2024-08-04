5fbcc784acb9fe9c2ffe062f2f694299e672cfce
OoHuakoO <komsark_@hotmail.co.th>
2024-08-04 12:40:52 +0700

    add get save data api

--- src/services/saveData.tsx
+++ src/services/saveData.tsx
@@ -1,5 +1,13 @@
-import { CreateCrabHatchParams } from '@src/typings/saveData';
-import { Response, post } from '@src/utils/axios';
+import {
+    CreateCrabHatchParams,
+    GetCrabHatchByIdResponse,
+    GetCrabHatchInquiryResponse,
+    GetWaterQualityAfterByIdResponse,
+    GetWaterQualityAfterInquiryResponse,
+    GetWaterQualityBeforeByIdResponse,
+    GetWaterQualityBeforeInquiryResponse
+} from '@src/typings/saveData';
+import { Response, get, post } from '@src/utils/axios';
 import FormData from 'form-data';
 
 export function CreateWaterQualityBefore(
@@ -18,4 +26,48 @@ export function CreateCrabHatch(
     params: CreateCrabHatchParams
 ): Promise<Response<any>> {
     return post<any>('/crab-hatch/createCrabHatch', params);
+}
+
+export function GetWaterQualityBeforeInquiry(): Promise<
+    Response<GetWaterQualityBeforeInquiryResponse[]>
+> {
+    return get<GetWaterQualityBeforeInquiryResponse[]>(
+        '/water-quality-before/getWaterQualityBefore'
+    );
+}
+
+export function GetWaterQualityAfterInquiry(): Promise<
+    Response<GetWaterQualityAfterInquiryResponse[]>
+> {
+    return get<GetWaterQualityAfterInquiryResponse[]>(
+        '/water-quality-after/getWaterQualityAfter'
+    );
+}
+
+export function GetCrabHatchInquiry(): Promise<
+    Response<GetCrabHatchInquiryResponse[]>
+> {
+    return get<GetCrabHatchInquiryResponse[]>('/crab-hatch/crabHatchAll');
+}
+
+export function GetWaterQualityBeforeById(
+    id
+): Promise<Response<GetWaterQualityBeforeByIdResponse>> {
+    return get<GetWaterQualityBeforeByIdResponse>(
+        `/water-quality-before/getWaterQualityBeforeById/${id}`
+    );
+}
+
+export function GetWaterQualityAfterById(
+    id
+): Promise<Response<GetWaterQualityAfterByIdResponse>> {
+    return get<GetWaterQualityAfterByIdResponse>(
+        `/water-quality-after/getWaterQualityAfterById/${id}`
+    );
+}
+
+export function GetCrabHatchById(
+    id
+): Promise<Response<GetCrabHatchByIdResponse>> {
+    return get<GetCrabHatchByIdResponse>(`/crab-hatch/crabHatchById/${id}`);
 }
--- src/typings/saveData.tsx
+++ src/typings/saveData.tsx
@@ -17,3 +17,48 @@ export interface CreateCrabHatchParams {
     crabEggScoopDate: string;
     crabReleaseDate: string;
 }
+
+export interface GetWaterQualityBeforeInquiryResponse {
+    _id: string;
+    createdAt: string;
+}
+
+export interface GetWaterQualityAfterInquiryResponse {
+    _id: string;
+    createdAt: string;
+}
+
+export interface GetCrabHatchInquiryResponse {
+    _id: string;
+    createdAt: string;
+}
+
+export interface GetWaterQualityBeforeByIdResponse {
+    location: string;
+    pool: string;
+    salinity: string;
+    ph: string;
+    alkaline: string;
+    salinityImg: string;
+    phImg: string;
+    alkalineImg: string;
+}
+
+export interface GetWaterQualityAfterByIdResponse {
+    location: string;
+    pool: string;
+    ammonia: string;
+    calcium: string;
+    magnesium: string;
+    ammoniaImg: string;
+    calciumImg: string;
+    magnesiumImg: string;
+}
+
+export interface GetCrabHatchByIdResponse {
+    location: string;
+    pool: string;
+    crabEggColor: string;
+    crabReleaseDate: string;
+    crabEggScoopDate: string;
+}
