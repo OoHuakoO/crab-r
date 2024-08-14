a20ecee4828442879c8fb0cb0091dfcc7ecd6815
YongSamita <samita1999nun@gmail.com>
2024-08-15 03:47:43 +0700

    add setup ios

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
 import React, { useEffect } from 'react';
-import { PermissionsAndroid } from 'react-native';
+import { PermissionsAndroid, Platform } from 'react-native';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-native-paper';
 import { RecoilRoot } from 'recoil';
@@ -11,9 +11,11 @@ import { theme } from './src/theme';
 function Main() {
     useEffect(() => {
         const requestUserPermission = async () => {
-            PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
-            );
+            if (Platform.OS === 'android') {
+                PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+                );
+            }
             const authStatus = await messaging().requestPermission();
             const enabled =
                 authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
@@ -21,6 +23,7 @@ function Main() {
 
             if (enabled) {
                 const token = await messaging().getToken();
+                console.log('token', token);
                 await AsyncStorage.setItem('FcmToken', JSON.stringify(token));
             }
         };
