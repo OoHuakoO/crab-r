38cf699859be0f9fde54715bcf763ca4d55ad12b
YongSamita <samita1999nun@gmail.com>
2024-09-04 01:29:46 +0700

    fix splash screen ios

--- ios/CrabR/AppDelegate.mm
+++ ios/CrabR/AppDelegate.mm
@@ -10,10 +10,13 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  BOOL ret = [super application:application didFinishLaunchingWithOptions:launchOptions];
   [FIRApp configure];
-  [RNSplashScreen show]; 
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+   if (ret == YES)
+  {
+    [RNSplashScreen show];
+  }
+  return ret;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
