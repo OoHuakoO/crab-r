c5773289ba0142dd0f8a85e84856aec767035bd4
YongSamita <samita1999nun@gmail.com>
2024-09-13 02:57:28 +0700

    add tokenWithCompletion

--- ios/CrabR/AppDelegate.mm
+++ ios/CrabR/AppDelegate.mm
@@ -1,30 +1,41 @@
 #import "AppDelegate.h"
 #import "RNSplashScreen.h"
 #import <React/RCTBundleURLProvider.h>
+#import <UserNotifications/UserNotifications.h>
+#import <Firebase.h>
+#import <FirebaseMessaging.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"CrabR";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
   BOOL ret = [super application:application didFinishLaunchingWithOptions:launchOptions];
   [FIRApp configure];
   [application registerForRemoteNotifications];
-   if (ret == YES)
+  if (ret == YES)
   {
     [RNSplashScreen show];
   }
   return ret;
 }
 
-- (void)application:(UIApplication *)application
- didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
- [FIRMessaging messaging].APNSToken = deviceToken;
+// Set APNS Token
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [FIRMessaging messaging].APNSToken = deviceToken;
+  // Request FCM token
+  [[FIRMessaging messaging] tokenWithCompletion:^(NSString * _Nullable token, NSError * _Nullable error) {
+    if (error != nil) {
+      NSLog(@"Error fetching FCM token: %@", error.localizedDescription);
+    } else {
+      NSLog(@"FCM Token: %@", token);
+    }
+  }];
 }
 
+// Get Bundle URL
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
   return [self bundleURL];
